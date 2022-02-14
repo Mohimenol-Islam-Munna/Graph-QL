d55967dc5616a57682f60384b68e337817f9bdb4
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-14 12:28:12 +0600

    graphql schema, resolver, query

---
+++ server.js
@@ -0,0 +1,29 @@
+const { buildSchema, graphql } = require("graphql");
+
+// build schema
+const schema = buildSchema(` 
+    type Query {
+        name: String,
+        email: String,
+    }
+`);
+
+// resolver(object of fucntion)
+// * method's name in resolver must be match the any name of Query's property in buildSchema
+const rootValue = {
+  name: () => "Mohimenol Islam Munna",
+  email: "munna.cse.pust@gmail.com",
+};
+
+// query
+graphql({
+  // schema name
+  schema,
+
+  // query
+  //   source name must be match the any name of Query's property in buildSchema
+  source: "{name, email}",
+
+  // resolver
+  rootValue,
+}).then((res) => console.log("Query Response ::", res));
